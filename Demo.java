0456056c3d20b6657d4fb624e9bab53d6823cde3
Em <ltmf1990@protonmail.com>
2022-11-27 21:28:57 -0500

    Final touches

--- Demo.java
+++ Demo.java
@@ -1,4 +1,3 @@
-import java.lang.Class;
 import java.lang.reflect.InvocationTargetException;
 
 public class Demo
@@ -6,19 +5,29 @@ public class Demo
   public static void main(String[] args) 
     throws NoSuchMethodException, SecurityException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, ClassNotFoundException
   {
+
+
       SingletonA test;
-      test = (SingletonA)Multiton.GetInstance((Class<Multiton>)Class.forName("SingletonA"), "hola");
+      test = (SingletonA)Multiton.GetInstance("SingletonA", "hola");
       test.saySecret();
+      //Doesn't get created because Multiton already contains a SingletonA instance
       SingletonA test2;
-      test2 = (SingletonA)Multiton.GetInstance((Class<Multiton>)Class.forName("SingletonA"), "bye bye");
+      test2 = (SingletonA)Multiton.GetInstance("SingletonA", "bye bye");
       test2.saySecret();
 
       SingletonB test3;
-      test3 = (SingletonB)Multiton.GetInstance((Class<Multiton>)Class.forName("SingletonB"), "hello");
+      test3 = (SingletonB)Multiton.GetInstance(SingletonB.class.getSimpleName(), "hello");
       test3.saySecret();
+      //Doesn't get created because Multiton already contains a SingletonB instance
       SingletonB test4;
-      test4 = (SingletonB)Multiton.GetInstance((Class<Multiton>)Class.forName("SingletonB"), "goodbye");
+      test4 = (SingletonB)Multiton.GetInstance(SingletonB.class.getSimpleName(), "goodbye");
       test4.saySecret();
+
+      //Doesn't get created because RandomObj isn't derived from Multiton
+      SingletonA notAValidSingleton;
+      notAValidSingleton = (SingletonA)Multiton.GetInstance("RandomObj", "denied");
+
   }
 
 }
+
--- Multiton.java
+++ Multiton.java
@@ -4,21 +4,18 @@ import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.Class;
 
-abstract public class Multiton
-{
+abstract public class Multiton {
 
   private static final HashMap<String, Multiton> _instances = new HashMap<>();
   protected String singletonTester;
 
-  protected Multiton(String secret)
-  {
+  protected Multiton(String secret) {
     singletonTester = secret;
   }
 
   // Getters
 
-  private static HashMap<String, Multiton> getInstances()
-  {
+  private static HashMap<String, Multiton> getInstances() {
     return _instances;
   }
 
@@ -28,31 +25,67 @@ abstract public class Multiton
     System.out.println(singletonTester);
   }
 
-  public static Multiton GetInstance(Class<Multiton> singletonType, String secret) 
-    throws NoSuchMethodException, SecurityException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException
+  /* Method to get a specific singleton by class name. If the name is not a valid child of Multiton,
+  the function will return null!
+  Secret can be changed for whatever state to pass into the Singleton's constructor */
+  public static Multiton GetInstance(String singletonName, String secret)
+      throws ClassNotFoundException, NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException,
+      InvocationTargetException 
   {
     final ReentrantLock multitonLock = new ReentrantLock();
     multitonLock.lock();
-    try
+    try 
     {
-
       Multiton instance;
-
-      if(getInstances().containsKey(singletonType.getSimpleName()) == false)
+      Constructor<?> singletonConstructor;
+      try 
       {
+        Class<?> singletonType = Class.forName(singletonName);
+        if (getInstances().containsKey(singletonType.getSimpleName()) == false)
+        {
           Class<?>[] parameterTypes = { String.class };
-          Constructor<Multiton> singletonConstructor = singletonType.getDeclaredConstructor(parameterTypes);
-          instance = singletonConstructor.newInstance(secret);
+          singletonConstructor = singletonType.getDeclaredConstructor(parameterTypes);
+          instance = (Multiton) singletonConstructor.newInstance(secret);
           getInstances().put(singletonType.getSimpleName(), instance);
-
+        }
+        return (Multiton) getInstances().get(singletonType.getSimpleName());
+      }
+      catch (ClassNotFoundException e)
+      {
+        System.out.println(e.getClass().getSimpleName() + ": You requested an invalid singleton!");
+        return null;
+      } 
+      catch (NoSuchMethodException e) 
+      {
+        System.out.println(e.getClass().getSimpleName() + ": You requested an invalid singleton!");
+        return null;
+      } 
+      catch (InstantiationException e) 
+      {
+        System.out.println(e.getClass().getSimpleName() + ": You requested an invalid singleton!");
+        return null;
+      } 
+      catch (IllegalArgumentException e) 
+      {
+        System.out.println(e.getClass().getSimpleName()
+            + ": Something went wrong during the creation of the singleton's constructor.");
+        return null;
+      } 
+      catch (SecurityException e) 
+      {
+        System.out.println(e.getClass().getSimpleName() + ": Uh oh! Something bad happened!");
+        return null;
+      } 
+      catch (IllegalAccessException e)
+      {
+        System.out.println(e.getClass().getSimpleName() + ": Uh oh! Something bad happened!");
+        return null;
       }      
-      return getInstances().get(singletonType.getSimpleName());
     }
-    finally
+    finally 
     {
-      multitonLock.unlock();
+      multitonLock.unlock();    
     }
-    
   }
-
 }
+
